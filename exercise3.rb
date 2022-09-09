9c2095cbcc53917822c2ac63a0dc1040aa08e4f5
Supermike <superdafe@gmail.com>
2022-09-09 14:24:49 +0100

    finish exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,9 +1,19 @@
 class Person
-  attr_accessor :id, :name, :age
-  
+  attr_accessor :name, :this_children
+  def initialize(name=nil, parent=nil)
+    @name = name
+    @parent = parent
+    self.this_children = []
+    if !@parent.nil?
+      @parent.this_children << self
+    end
+  end
+
   def children
+    self.this_children.map(&:name)
   end
 
   def grandchildren
+    self.this_children.map{|child| child.children}.flatten!
   end
 end
